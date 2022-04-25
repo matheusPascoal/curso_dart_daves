00f32a62b8cd6e21e149138ea4839794ff3be59b
matheusPascoal <pascoalleander@gmail.com>
2022-04-25 00:25:22 -0300

    Curso Daves

--- opeMatmaticas.dart
+++ opeMatmaticas.dart
@@ -1,3 +1,21 @@
-void main() {}
+void main() {
+  int n1 = 10;
+  int n2 = 5;
+
+  //ADIÇÃO
+  print('ADIÇÃO =  ${n1 + n2}');
+  //SUBTRAÇÃO
+  print('SUBTRAÇÃO = ${n1 - n2}');
+  //DIVISÃO PARTE INTEIRA
+  print('DIVISÃO PARTE INTEIRA = ${n1 ~/ n2}');
+  //RESTO
+  print('RESTO = ${n1 % n2}');
+  //DIVISÃO
+  print('DIVISÃO = ${n1 / n2}');
+  //MULTIPLICAÇÃO
+  print('MULTIPLICAÇÃO = ${n1 * n2}');
+  //CALCULO
+  print('CALCULO SIMPLES = ${n1 + (n2 * n2)}');
+}
 
 //dart
--- treinoVariaveis.dart
+++ treinoVariaveis.dart
@@ -1,27 +1,18 @@
 //CONCATENAÇÃO DE VARIAVEIS
 void main() {
   String texto = 'Texto';
+  String text = 'oi';
   int inteiro = 10;
-  double fracao = 1.5;
+  double numReal = 1.5;
   bool vf = true;
   num numeros = 1.100;
   dynamic aleatorio = 'Aceita qualquer valor e muda quando quiser';
   var naoMudaTipo = 'Aceita qualquer valor numeros ou texto mas nao se muda';
 
-  print('===============================');
-  var x = 'GALO';
-  print('Ultimo campeao BRASILEIRO? $x');
-  print('===============================');
-  // USA CHAVES NESSE CASO PARA FAZER A MULTIPLICAÇÃO NO CASO MAS FUNCIONA PARA OUTRAS COISAS
-  print('Quanto e 10 x 5 = ${inteiro * 5}');
-  print('===============================');
-  print('Isso esta certo ? $vf. Quanto custa? $numeros');
-  print('===============================');
-  print('===============================');
-  // QUANDO NAO FOR TEXTO PODE SE USAR O toSTRING
-  print('Outra forma de concatenar ' +
-      fracao.toString() +
-      ' isso é ' +
-      vf.toString());
+  //CONCATENANDO
+  print('Este e um numero real = $numReal');
+  print('Multiplicação $numReal x 2 = ${numReal * 2}');
+  //toString > CONVERTE PARA STRING
+  print('Conveção de um int para String = ' + inteiro.toString());
 }
 //dart
